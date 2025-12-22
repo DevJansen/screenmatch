be0bc437fdbd83eee581f5f3fccc756df5236afa
jhonata jansen <codejhonatajansen@gmail.com>
2025-12-21 23:43:41 -0300

    commit inicial

---
+++ src/test/java/com/devjansen/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjansen.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
